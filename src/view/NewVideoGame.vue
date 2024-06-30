380fa936d302187811f2dfd7c9edf85c32a71d4a
Julius <julius.demartini@gmail.com>
2024-06-30 20:10:01 -0300

    feat: videogame and new videogame pages

---
+++ src/view/NewVideoGame.vue
@@ -0,0 +1,60 @@
+<template>
+    <header>
+        <router-link :to="{ name: 'index' }"><h1>Capybara Games</h1></router-link>
+        <nav>
+            <ul>
+                <li><router-link :to="{ name: 'index' }">Home</router-link></li>
+                <li><router-link :to="{ name: 'videogames-list' }">Lista de Videojuegoss</router-link></li>
+                <li><router-link :to="{ name: 'contact' }">Contacto</router-link></li>
+            </ul>
+        </nav>
+        <aside id="aside">
+            <ul>
+                <li><router-link :to="{ name: 'index' }">Home</router-link></li>
+                <li><router-link :to="{ name: 'videogames-list' }">Lista de Videojuegoss</router-link></li>
+                <li><router-link :to="{ name: 'contact' }">Contacto</router-link></li>
+            </ul>
+        </aside>
+    </header>
+    <main>
+        <h2>Agregar videojuego</h2>
+        <form class="newgame-form" method="post" onsubmit="addNewVideogame(event)">
+            <label for="title">Titulo<input type="text" id="title" name="title" placeholder="Nombre"></label> 
+            <label for="description">Descripción<textarea id="description" name="description" placeholder="Descripción"></textarea></label>
+            <ul id="platform">
+                <li>Crossplay</li>
+                <li><input type="radio" id="mobile" name="mobile" value="1">
+                <label for="mobile">Mobile</label></li>
+                <li><input type="radio" id="desktop" name="desktop" value="2">
+                <label for="desktop">Escritorio</label></li>
+                <li><input type="radio" id="multiplatform" name="multiplatform" value="3">
+                <label for="multiplatform">Multiplatforma</label></li>
+            </ul> 
+            <label for="category">Categoría<select id="category" name="category">
+                <option value="">Elige una categoría.</option>
+                <option value="1">Plataformas</option>
+                <option value="2">Mundo abierto</option>
+                <option value="3">2D</option>
+                <option value="4">3D</option>
+            </select></label>
+            <label for="image">Imagen<input type="file" id="image" name="image"></label> 
+            <label for="url">URL de trailer<input type="url" id="url" name="url" placeholder="https://example.com"></label> 
+            <div class="submit-button">
+                <label for="new-videogame-submit" class="submit-label-button">ENVIAR</label>
+                <button style="display: none;" id="new-videogame-submit" type="submit" value="Submit"></button>
+            </div>
+        </form>
+    </main>
+    <footer>
+        <p>Derechos de autor © 2024 CapybaraGames</p>
+        <p>Contáctanos en info@capybaragames.com</p>
+    </footer>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ src/view/VideoGame.vue
@@ -0,0 +1,51 @@
+<template>
+    <header>
+        <router-link :to="{ name: 'index' }"><h1>Capybara Games</h1></router-link>
+        <nav>
+            <ul>
+                <li><router-link :to="{ name: 'index' }">Home</router-link></li>
+                <li><router-link :to="{ name: 'videogames-list' }">Lista de Videojuegoss</router-link></li>
+                <li><router-link :to="{ name: 'contact' }">Contacto</router-link></li>
+            </ul>
+        </nav>
+        <aside id="aside">
+            <ul>
+                <li><router-link :to="{ name: 'index' }">Home</router-link></li>
+                <li><router-link :to="{ name: 'videogames-list' }">Lista de Videojuegoss</router-link></li>
+                <li><router-link :to="{ name: 'contact' }">Contacto</router-link></li>
+            </ul>
+        </aside>
+    </header>
+    <main>
+        <h2>Celeste</h2>
+        <article class="details-videogame">
+            <div class="details-img">
+                <img src="/img/celeste_logo.png" alt="">
+                <div class="details-info">
+                    <h3>Categoría:</h3>
+                    <p>Plataformas</p>
+                    <h3>Plataforma:</h3>
+                    <p>Escritorio</p>
+                    <h3>Trailer:</h3>
+                    <a href="https://www.youtube.com/watch?v=70d9irlxiB4">Ver online</a>
+                </div>
+            </div>
+            <div class="details-description">
+                <h3>Descripción:</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis labore odit, nobis laudantium quibusdam soluta laboriosam doloremque nesciunt aut odio ut assumenda rerum veniam ullam mollitia fugiat dolorem optio excepturi facilis dicta unde modi neque! Dolorum et fugiat adipisci atque debitis asperiores voluptas non repellendus, voluptatum nostrum perspiciatis. Quod, sint?</p>
+            </div>
+        </article>
+    </main>
+    <footer>
+        <p>Derechos de autor © 2024 CapybaraGames</p>
+        <p>Contáctanos en info@capybaragames.com</p>
+    </footer>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
