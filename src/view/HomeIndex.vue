e7ee77620104d693df6ee7fc54c51a80df76bd18
Maty910 <matychacong@gmail.com>
2024-07-01 13:21:09 -0300

    feat & fix: fav icon and home about us

--- src/view/HomeIndex.vue
+++ src/view/HomeIndex.vue
@@ -21,10 +21,10 @@
         </div>
         <article>
             <h2>About us</h2>
-            <p>Bienvenido a Capybara Games, tu fuente definitiva para descubrir y explorar el emocionante mundo de los videojuegos. Nuestra misión es brindar a los jugadores de todas las edades y niveles de experiencia una plataforma donde puedan encontrar información detallada y actualizada sobre los juegos más populares y los próximos lanzamientos.
-En Capybara Games, creemos que cada juego tiene una historia única que contar. Por eso, nos dedicamos a ofrecer descripciones completas que te sumergen en el universo de cada título. Desde los trailers más recientes hasta la información esencial sobre plataformas, géneros y características, nuestra página está diseñada para proporcionarte todo lo que necesitas saber para elegir tu próxima aventura digital.
-Nuestro equipo de apasionados gamers y expertos en la industria trabaja incansablemente para mantenerte al día con las últimas novedades y tendencias del mundo de los videojuegos. Ya sea que busques un análisis profundo de un juego, una visión general rápida o simplemente quieras ver el último trailer, Capybara Games es el lugar ideal para satisfacer tu curiosidad y alimentar tu pasión por los videojuegos.
-¡Únete a nuestra comunidad y explora con nosotros el fascinante universo de los videojuegos!</p>
+            <p>Bienvenido a Capybara Games, tu fuente definitiva para descubrir y explorar el emocionante mundo de los videojuegos. Nuestra misión es brindar a los jugadores de todas las edades y niveles de experiencia una plataforma donde puedan encontrar información detallada y actualizada sobre los juegos más populares y los próximos lanzamientos. <br> <br>
+            En Capybara Games, creemos que cada juego tiene una historia única que contar. Por eso, nos dedicamos a ofrecer descripciones completas que te sumergen en el universo de cada título. Desde los trailers más recientes hasta la información esencial sobre plataformas, géneros y características, nuestra página está diseñada para proporcionarte todo lo que necesitas saber para elegir tu próxima aventura digital. <br> <br>
+            Nuestro equipo de apasionados gamers y expertos en la industria trabaja incansablemente para mantenerte al día con las últimas novedades y tendencias del mundo de los videojuegos. Ya sea que busques un análisis profundo de un juego, una visión general rápida o simplemente quieras ver el último trailer, Capybara Games es el lugar ideal para satisfacer tu curiosidad y alimentar tu pasión por los videojuegos. <br> <br>
+            ¡Únete a nuestra comunidad y explora con nosotros el fascinante universo de los videojuegos!</p>
         </article>
     </main>
     <FooterComp />
